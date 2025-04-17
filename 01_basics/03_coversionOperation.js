de36f4a7d441d035a28adfb240b9d24aad83ddc5
Ashapr100 <akashgarimella04@gmail.com>
2025-04-17 12:13:10 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_coversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33abcfshedbje";
+// let score = undefined (Nan)
+// let score = "Akash" (Nan)
+// let score = true; boolean value,if false then it will be 0
+// let score = null; value is 0 of null
+// console.log(typeof score);
+// // console.log(typeof (score));
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// Nan = "Not a Number" full form
+
+// "33" => 33
+// "33abc" => NaN
+// True =>; false => 0
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1=> true; 0 => false
+// "" => false
+// "Akash" => true
+
+let someNumber = 33;
+
+let stringNumber = String(SomeNumber);
+console.log(StringNumber);
+console.log(typeof stringNumber);
