9f68c1d2258db27626811258e3b34f4c3bbc57bb
Ashapr100 <akashgarimella04@gmail.com>
2025-04-17 04:08:20 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS CODE as newer version
+
+// alert(3+3)  we are using node js, not browser
+
+console.log(3 + 3); // code readability should be high
+
+console.log("Akash");
+
+// let name = "Akash"; String
+// let age = 17; Number
+// let isLoggedIn = false; boolesn datatypes
+// let sate;
+
+// number => to power 53
+// bigint
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+// console.log(typeof "Akash"); node 01_basics/02_dataTypes.js
+console.log(typeof null);
+
+// null is an object basically
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
